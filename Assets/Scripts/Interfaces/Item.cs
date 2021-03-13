043baa593da9ef9f4732ee5635d7edd9523b2859
RzRaul <rodriguez.raul54@uabc.edu.mx>
2021-03-12 17:05:17 -0800

    Interfaces creadas

---
+++ Assets/Scripts/Interfaces/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public int id;
+    public string name;
+    public Sprite icon;
+    public string description;
+    public virtual void use(){};
+
+
+}
